3446f9218d6dc16b33ea4104461ef42bee60cf29
V <01186948@pw.edu.pl>
2026-01-11 23:27:30 +0100

    Function for printing the tree, deleted redundant class element

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
 from file_parsing.FileParser import FileParser as file_parser
-from algorithms.DecisionTree import DecisionTree
+from algorithms.DecisionTree import DecisionTree, print_tree
+from contextlib import redirect_stdout
 
 def main():
-    #fp = file_parser("data/spliceDTrainKIS.dat.txt")
-    #dt = DecisionTree(fp.parse(), 5)
-    #root = dt.train()
-    training_data = [
-        (1, "AAGCT"),
-        (1, "AAGGT"),
-        (0, "CCGCT"),
-        (0, "TTGCT"),
-        (1, "ACCCC"),
-    ]
+    depth = 35
+    fp = file_parser("data/spliceDTrainKIS.dat.txt")
+    training_data = fp.parse()
 
-    dt = DecisionTree(training_data, max_depth=3)
+    dt = DecisionTree(training_data, max_depth=depth)
     root = dt.train()
 
-    print("--- WYNIK ---")
-    print(f"Najlepsze pierwsze pytanie: Sprawdź pozycję {root.attribute_pos}")
-    print(f"Jeśli litera to {root.split_condition_group} -> Idź w LEWO")
-    print(f"W przeciwnym razie -> Idź w PRAWO")
+    print("--- BUDOWA DRZEWA ---")
+
+    # Wywołujemy naszą nową funkcję wizualizującą
+    with open('outTree.txt', 'w') as f:
+        with redirect_stdout(f):
+            print_tree(root, 0)
+
 main()
